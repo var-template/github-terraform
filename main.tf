a69caeedb63bab4b02067afaf83e2616f2cd9c73
Tsuji Riya <tsuji.riya@gmail.com>
2026-07-08 21:03:27 +0900

    feat: Create base of terraform (#8)
    * Create base of terraform
    Co-authored-by: proto08 <194586153+proto08@users.noreply.github.com>
    
    * Revert "Create base of terraform"
    
    This reverts commit 551b13965d90676f1877cbea65f1d95c1d0dbe7a.
    
    * Create base terraform
    
    Co-Authored-By: proto08 <194586153+proto08@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: proto08 <194586153+proto08@users.noreply.github.com>

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+locals {
+  github_organization = "var-template"
+  app_id = "4244361"
+  app_installation_id = "Iv23ctKOEm5D6o1ooeyq"
+}
